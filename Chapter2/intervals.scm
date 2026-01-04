e64ad6d58b8ef0f601866aec0dbae8b70f42c98f
SpecificMachine1 <fjt72701@yahoo.com>
2026-01-04 13:38:16 -0500

    fix width

--- Chapter2/intervals.scm
+++ Chapter2/intervals.scm
@@ -46,7 +46,7 @@
 (define (center i)
   (/ (+ (upper-bound i) (lower-bound i)) 2))
 (define (width i)
-  (/ (- (lower-bound i) (upper-bound i)) 2))
+  (/ (- (upper-bound i) (lower-bound i)) 2))
 
 (define (make-center-percent center percent)
   (make-center-width center (/ (* center percent) 100)))
